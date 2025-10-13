2771a0bcc57beaed6082b13e7528ef34948fc6e5
Rosyid Eko Nugroho <rosyid123.ren@gmail.com>
2025-10-13 16:43:49 +0700

    feat(template): setup and restructure views

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,78 @@
+<nav class="nxl-navigation">
+    <div class="navbar-wrapper">
+        <div class="m-header">
+            <a href="index.html" class="b-brand" style="display: flex; align-items: center; gap: 8px;">
+                <div class="logo logo-lg">
+                    <img src="{{ asset('assets/images/logo/logo.jpeg') }}" alt="Logo" class="logo logo-lg"
+                        style="height: 80px; width: auto;">
+                        <span><b>Sistem Admin</b></span>
+                </div>
+                <img src="{{ asset('assets/images/logo/logo.jpeg') }}" alt="" class="logo logo-sm" />
+            </a>
+        </div>
+        <div class="navbar-content">
+            <ul class="nxl-navbar">
+                <li class="nxl-item nxl-caption">
+                    <label>Master Data</label>
+                </li>
+                <li class="nxl-item nxl-hasmenu">
+                    <a href="javascript:void(0);" class="nxl-link">
+                        <span class="nxl-micon"><i class="feather-lock"></i></span>
+                        <span class="nxl-mtext">Manajemen User</span><span class="nxl-arrow"><i
+                                class="feather-chevron-right"></i></span>
+                    </a>
+                    <ul class="nxl-submenu">
+                        <li class="nxl-item"><a class="nxl-link" href="#">User</a></li>
+                        <li class="nxl-item"><a class="nxl-link" href="#">Peran</a></li>
+                        <li class="nxl-item"><a class="nxl-link" href="#">Izin Akses</a></li>
+                        <li class="nxl-item"><a class="nxl-link" href="#">Pengaturan Peran & Izin</a></li>
+                    </ul>
+
+                </li>
+                <li class="nxl-item nxl-caption">
+                    <label>Navigation</label>
+                </li>
+                <li class="nxl-item nxl-hasmenu">
+                    <a href="#" class="nxl-link">
+                        <span class="nxl-micon"><i class="feather-airplay"></i></span>
+                        <span class="nxl-mtext">Dashboard</span>
+                    </a>
+                </li>
+                <li class="nxl-item nxl-hasmenu">
+                    <a href="javascript:void(0);" class="nxl-link">
+                        <span class="nxl-micon"><i class="feather-users"></i></span>
+                        <span class="nxl-mtext">Customer</span><span class="nxl-arrow"><i
+                                class="feather-chevron-right"></i></span>
+                    </a>
+                    <ul class="nxl-submenu">
+                        <li class="nxl-item"><a class="nxl-link" href="#">Data Customer</a></li>
+                        <li class="nxl-item"><a class="nxl-link" href="#">Transaksi Pembelian Customer</a></li>
+                    </ul>
+                </li>
+                <li class="nxl-item nxl-hasmenu">
+                    <a href="javascript:void(0);" class="nxl-link">
+                        <span class="nxl-micon"><i class="feather-briefcase"></i></span>
+                        <span class="nxl-mtext">Sales</span><span class="nxl-arrow"><i
+                                class="feather-chevron-right"></i></span>
+                    </a>
+                    <ul class="nxl-submenu">
+                        <li class="nxl-item"><a class="nxl-link" href="#">Data Penjualan Sales</a></li>
+                        <li class="nxl-item"><a class="nxl-link" href="#">Surat Penawaran</a></li>
+                        <li class="nxl-item"><a class="nxl-link" href="#">Surat Agreement</a></li>
+                    </ul>
+                </li>
+                <li class="nxl-item nxl-hasmenu">
+                    <a href="javascript:void(0);" class="nxl-link">
+                        <span class="nxl-micon"><i class="feather-gift"></i></span>
+                        <span class="nxl-mtext">Promo & Produk</span><span class="nxl-arrow"><i
+                                class="feather-chevron-right"></i></span>
+                    </a>
+                    <ul class="nxl-submenu">
+                        <li class="nxl-item"><a class="nxl-link" href="#">Program Promo</a></li>
+                        <li class="nxl-item"><a class="nxl-link" href="#">Pricelist Produk</a></li>
+                        <li class="nxl-item"><a class="nxl-link" href="#">Form Klaim Produk</a></li>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
